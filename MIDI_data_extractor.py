cd7849a607f485d0e021b5a68bd9ea7b1f6256a6
Ryan Kim <ryankim17920@gmail.com>
2023-07-12 22:10:34 -0500

    Added pitchwheel, aftertouch, and polytouch
    Changed/Modified:
    
    I added all three of these to the data_extractor but they are pretty much useless (none of my data have it), so this commit exists purely for the future, if I attempt to generalize this data extractor
    - removed time_inc and print_ because they're useless

--- MIDI_data_extractor.py
+++ MIDI_data_extractor.py
@@ -3,14 +3,13 @@ import numpy as np
 from index_based_matrix_appender import index_based_matrix_appender
 from add_column_to_2d_array import add_column_to_2d_array
 from tqdm import tqdm
-def MIDI_data_extractor(midi_file_path, time_inc=True, print_ = False):
+def MIDI_data_extractor(midi_file_path):
     np.set_printoptions(threshold=np.inf)
     np.set_printoptions(linewidth=np.inf)
     midi_file = MidiFile(midi_file_path)
     matrix = np.array([], dtype=np.int16)
     used_instruments = np.zeros(128)
     track_rp = -1
-    print(midi_file.ticks_per_beat)
     for i, track in tqdm(enumerate(midi_file.tracks)):
         print('Track {}: {}'.format(i, track.name))
         track_matrix = np.array([], dtype=np.int16)
@@ -21,19 +20,19 @@ def MIDI_data_extractor(midi_file_path, time_inc=True, print_ = False):
         if track_rp != -1:
             program_matrix = np.append(program_matrix, [[0], [track_rp]])
             program_matrix = np.reshape(program_matrix, (-1, 2))
-            msg_array = np.full(15, -1)
+            msg_array = np.full(18, -1)
             msg_array[-1] = 0
             msg_array[6] = track_rp
             track_matrix = np.append(track_matrix, [msg_array])
             orig_instr = track_rp
             track_rp = -1
-            #print(program_matrix)
-
         for msg in track:
             msg_counter += 1
-            msg_array = np.full(15, -1)
+            msg_array = np.full(18, -1)
             cur_time += msg.time
             msg_array[-1] = cur_time
+            #print(cur_time)
+            print(msg.type)
             if msg.type == 'note_on':
                 msg_array[0:2] = msg.note, msg.velocity
             elif msg.type == 'note_off':
@@ -50,18 +49,18 @@ def MIDI_data_extractor(midi_file_path, time_inc=True, print_ = False):
                 if msg.program <= 8 and track_rp == -1:
                     track_rp = msg.program
             elif msg.type == 'end_of_track':
-                eot_array = np.full(18, -1)
-                eot_array[-4] = msg.time
+                eot_array = np.full(21, -1)
+                eot_array[-4] = cur_time
                 eot_array[7] = 1
                 matrix = np.append(matrix, [eot_array])
             elif msg.type == 'set_tempo':
-                st_array = np.full(18, -1)
-                st_array[-4] = msg.time
+                st_array = np.full(21, -1)
+                st_array[-4] = cur_time
                 st_array[8] = msg.tempo
                 matrix = np.append(matrix, [st_array])
             elif msg.type == 'time_signature':
-                ts_array = np.full(18, -1)
-                ts_array[-4] = msg.time
+                ts_array = np.full(21, -1)
+                ts_array[-4] = cur_time
                 ts_array[9:13] = msg.numerator, msg.denominator, msg.clocks_per_click, msg.notated_32nd_notes_per_beat
                 matrix = np.append(matrix, [ts_array])
             elif msg.type == 'key_signature':
@@ -70,7 +69,12 @@ def MIDI_data_extractor(midi_file_path, time_inc=True, print_ = False):
                                 'D': 14, 'D#m': 15, 'Db': 16, 'Dm': 17, 'E': 18, 'Eb': 19, 'Ebm': 20, 'Em': 21, 'F': 22,
                                 'F#': 23, 'F#m': 24, 'Fm': 25, 'G': 26, 'G#m': 27, 'Gb': 28, 'Gm': 29}
                 msg_array[13] = key_sig_dict[msg.key]
-
+            elif msg.type == 'polytouch':
+                msg_array[14:16] = msg.note, msg.value
+            elif msg.type == 'aftertouch':
+                msg_array[16] = msg.value
+            elif msg.type == 'pitchwheel':
+                msg_array[17] = msg.pitch
             if not np.all(msg_array[0:-1] == -1):
                 track_matrix = np.append(track_matrix, [msg_array])
         track_matrix = track_matrix.astype(np.int64)
@@ -78,21 +82,20 @@ def MIDI_data_extractor(midi_file_path, time_inc=True, print_ = False):
             used_instruments[int(program_matrix[0][1])] += 1
             instr_num = used_instruments[int(program_matrix[0][1])]
             #print(instr_num)
-            track_matrix = track_matrix.reshape((-1, 15))
+            track_matrix = track_matrix.reshape((-1, 18))
             track_matrix = index_based_matrix_appender(track_matrix, program_matrix)
             track_matrix = add_column_to_2d_array(track_matrix, instr_num)
             track_matrix = add_column_to_2d_array(track_matrix, orig_instr)
             matrix = np.append(matrix, track_matrix)
-            matrix = matrix.reshape((-1, 18))
+            matrix = matrix.reshape((-1, 21))
             track_matrix = track_matrix.astype(np.int64)
         #print(track_matrix[0:100])
-    matrix = matrix.reshape((-1, 18))
+    matrix = matrix.reshape((-1, 21))
     matrix = np.unique(matrix, axis=0)
-    matrix = matrix.reshape((-1, 18))
+    matrix = matrix.reshape((-1, 21))
     matrix = matrix.astype(np.int64)
     #deprecated order: from first to last: Time, Program_change(Instrument), Tempo, time_sig, key_sig, control_change
-
-    cols_to_sort = [6, 8, 9, 13, 4]
+    cols_to_sort = [6, 8, 9, 13, 14, 16, 17, 4]
 
     # Initialize an empty list to hold the new rows
     new_rows = []
@@ -103,12 +106,19 @@ def MIDI_data_extractor(midi_file_path, time_inc=True, print_ = False):
         new_row_added = False
         # Iterate over the columns you want to split
         for col in cols_to_sort:
+            # Skip the iteration if the column is 18 (or -4)
+            if col == 18 or col == -4:
+                continue
             # Create a new row that is a copy of the original row
             new_row = row.copy()
             # Check if the value in the column is not -1
             if new_row[col] != -1:
+                this_time = new_row[-4]
                 # Set all other columns in cols_to_sort to -1
-                new_row[[i for i in cols_to_sort if i != col]] = -1
+                for i in cols_to_sort:
+                    if i != col:
+                        new_row[i] = -1
+                new_row[-4] = this_time
                 # Append the new row to the list of new rows
                 new_rows.append(new_row)
                 # Set the flag to True
@@ -121,24 +131,20 @@ def MIDI_data_extractor(midi_file_path, time_inc=True, print_ = False):
     matrix = np.vstack(new_rows)
 
     # Now sort by column 14 (time) in ascending order
-    index_time = np.argsort(matrix[:, 14], kind='stable')
+    index_time = np.argsort(matrix[:, -4], kind='stable')
     matrix = matrix[index_time]
 
     # Then sort by column 6 (value 6) - put all -1 at the end
     index_value_6 = np.argsort(matrix[:, 6] == -1, kind='stable')
     matrix = matrix[index_value_6]
 
-    if print_:
-        print(matrix)
-
-    if not time_inc:
-        matrix = np.delete(matrix, -4, axis=1)
     '''[note_on_note, note_on_velocity, note_off_note, note_off_velocity,
         control_change_control, control_change_value, program_change_program,
         end_of_track, set_tempo_tempo,
         time_sig_num, itme_sig_den, time_sig_clocksperclick, time_sig_notated_32nd,
-        key_sig(turn into numbers), [time (only shown during tests)], instrument_type, instrument_num, orig_instrument_number]'''
+        key_sig(turn into numbers), polytouch_note, polytouch_value, aftertouch_value, pitchwheel_pitch,
+        [time (only shown during tests)], instrument_type, instrument_num, orig_instrument_number]'''
 
     return matrix
 
-#MIDI_data_extractor(r"C:\Users\ilove\Downloads\Passacaglia_-_The_Impossible_Duet.mid", True, True)
+MIDI_data_extractor(r"C:\Users\ilove\Downloads\Bachs_Double_Concerto.mid")
