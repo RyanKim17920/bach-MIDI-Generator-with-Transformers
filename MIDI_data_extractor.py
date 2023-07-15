178fac9992e31d06c0973fe48660e45b8edaed81
Ryan Kim <ryankim17920@gmail.com>
2023-07-15 12:56:58 -0500

    Removed note_off related, new failed file
    Changes:
     - note_off could be represented as note_on with velocity 0, so it was removed to reduce redundancy
    - created data_to_numbers which was a failure since there is over a few trillion (or somewhat large amount) values that can occur, making this impossible. This was recorded for the learning process

--- MIDI_data_extractor.py
+++ MIDI_data_extractor.py
@@ -1,4 +1,4 @@
-from mido import MidiFile
+from mido import MidiFile, tempo2bpm
 import numpy as np
 from index_based_matrix_appender import index_based_matrix_appender
 from add_column_to_2d_array import add_column_to_2d_array
@@ -22,15 +22,15 @@ def MIDI_data_extractor(midi_file_path):
         if track_rp != -1:
             program_matrix = np.append(program_matrix, [[0], [track_rp]])
             program_matrix = np.reshape(program_matrix, (-1, 2))
-            msg_array = np.full(15, -1)
+            msg_array = np.full(13, -1)
             msg_array[-1] = 0
-            msg_array[6] = track_rp
+            msg_array[4] = track_rp
             track_matrix = np.append(track_matrix, [msg_array])
             orig_instr = track_rp
             track_rp = -1
         for msg in track:
             msg_counter += 1
-            msg_array = np.full(15, -1)
+            msg_array = np.full(13, -1)
             cur_time += msg.time
             msg_array[-1] = cur_time
             # print(cur_time)
@@ -38,11 +38,13 @@ def MIDI_data_extractor(midi_file_path):
             if msg.type == 'note_on':
                 msg_array[0:2] = msg.note, msg.velocity
             elif msg.type == 'note_off':
-                msg_array[2:4] = msg.note, msg.velocity
+                msg_array[0:2] = msg.note, 0
+                #note_off is unnecessary and should be removed
+            #deleted 2-4 hmm
             elif msg.type == 'control_change':
-                msg_array[4:6] = msg.control, msg.value
+                msg_array[2:4] = msg.control, msg.value
             elif msg.type == 'program_change':
-                msg_array[6] = msg.program
+                msg_array[4] = msg.program
                 # print(msg.program)
                 program_matrix = np.append(program_matrix, [[msg_counter], [msg.program]])
                 program_matrix = np.reshape(program_matrix, (-1, 2))
@@ -51,26 +53,26 @@ def MIDI_data_extractor(midi_file_path):
                 if msg.program <= 8 and track_rp == -1:
                     track_rp = msg.program
             elif msg.type == 'end_of_track':
-                eot_array = np.full(18, -1)
+                eot_array = np.full(16, -1)
                 eot_array[-4] = cur_time
-                eot_array[7] = 1
+                eot_array[5] = 1
                 matrix = np.append(matrix, [eot_array])
             elif msg.type == 'set_tempo':
-                st_array = np.full(18, -1)
+                st_array = np.full(16, -1)
                 st_array[-4] = cur_time
-                st_array[8] = msg.tempo
+                st_array[6] = tempo2bpm(msg.tempo) - 20
                 matrix = np.append(matrix, [st_array])
             elif msg.type == 'time_signature':
-                ts_array = np.full(18, -1)
+                ts_array = np.full(16, -1)
                 ts_array[-4] = cur_time
-                ts_array[9:13] = msg.numerator, msg.denominator, msg.clocks_per_click, msg.notated_32nd_notes_per_beat
+                ts_array[7:11] = msg.numerator, msg.denominator, msg.clocks_per_click, msg.notated_32nd_notes_per_beat
                 matrix = np.append(matrix, [ts_array])
             elif msg.type == 'key_signature':
                 key_sig_dict = {'A': 0, 'A#m': 1, 'Ab': 2, 'Abm': 3, 'Am': 4, 'B': 5,
                                 'Bb': 6, 'Bbm': 7, 'Bm': 8, 'C': 9, 'C#': 10, 'C#m': 11, 'Cb': 12, 'Cm': 13,
                                 'D': 14, 'D#m': 15, 'Db': 16, 'Dm': 17, 'E': 18, 'Eb': 19, 'Ebm': 20, 'Em': 21, 'F': 22,
                                 'F#': 23, 'F#m': 24, 'Fm': 25, 'G': 26, 'G#m': 27, 'Gb': 28, 'Gm': 29}
-                msg_array[13] = key_sig_dict[msg.key]
+                msg_array[11] = key_sig_dict[msg.key]
             if not np.all(msg_array[0:-1] == -1):
                 track_matrix = np.append(track_matrix, [msg_array])
         track_matrix = track_matrix.astype(np.int64)
@@ -78,20 +80,20 @@ def MIDI_data_extractor(midi_file_path):
             used_instruments[int(program_matrix[0][1])] += 1
             instr_num = used_instruments[int(program_matrix[0][1])]
             # print(instr_num)
-            track_matrix = track_matrix.reshape((-1, 15))
+            track_matrix = track_matrix.reshape((-1, 13))
             track_matrix = index_based_matrix_appender(track_matrix, program_matrix)
             track_matrix = add_column_to_2d_array(track_matrix, instr_num)
             track_matrix = add_column_to_2d_array(track_matrix, orig_instr)
             matrix = np.append(matrix, track_matrix)
-            matrix = matrix.reshape((-1, 18))
+            matrix = matrix.reshape((-1, 16))
             track_matrix = track_matrix.astype(np.int64)
         # print(track_matrix[0:100])
-    matrix = matrix.reshape((-1, 18))
+    matrix = matrix.reshape((-1, 16))
     matrix = np.unique(matrix, axis=0)
-    matrix = matrix.reshape((-1, 18))
+    matrix = matrix.reshape((-1, 16))
     matrix = matrix.astype(np.int64)
     # deprecated order: from first to last: Time, Program_change(Instrument), Tempo, time_sig, key_sig, control_change
-    cols_to_sort = [6, 8, 9, 13]
+    cols_to_sort = [4, 6, 7, 11]
 
     # Initialize an empty list to hold the new rows
     new_rows = []
@@ -102,8 +104,8 @@ def MIDI_data_extractor(midi_file_path):
         new_row_added = False
         # Iterate over the columns you want to split
         for col in cols_to_sort:
-            # Skip the iteration if the column is 15 (or -4)
-            if col == 15 or col == -4:
+            # Skip the iteration if the column is 12 (or -4)
+            if col == 12 or col == -4:
                 continue
             # Create a new row that is a copy of the original row
             new_row = row.copy()
@@ -126,20 +128,20 @@ def MIDI_data_extractor(midi_file_path):
     # Use numpy.vstack to stack the list of new rows into a new matrix
     matrix = np.vstack(new_rows)
 
-    # Now sort by column 14 (time) in ascending order
+    # Now sort by column -4 (time) in ascending order
     index_time = np.argsort(matrix[:, -4], kind='stable')
     matrix = matrix[index_time]
 
-    # Then sort by column 6 (value 6) - put all -1 at the end
-    index_value_6 = np.argsort(matrix[:, 6] == -1, kind='stable')
-    matrix = matrix[index_value_6]
+    # Then sort by column 4 (value 4) - put all -1 at the end
+    index_value_4 = np.argsort(matrix[:, 4] == -1, kind='stable')
+    matrix = matrix[index_value_4]
 
     '''[note_on_note, note_on_velocity, note_off_note, note_off_velocity,
         control_change_control, control_change_value, program_change_program,
         end_of_track, set_tempo_tempo,
         time_sig_num, itme_sig_den, time_sig_clocksperclick, time_sig_notated_32nd,
-        key_sig(turn into numbers), [time], instrument_type, instrument_num, orig_instrument_number]'''
+        key_sig(turn into numbers), [time], instrument_type, instrument_num, orig_instrument_type]'''
 
     return matrix
 
-# MIDI_data_extractor(r"")
+#print(MIDI_data_extractor(r"test.mid"))
--- data_to_MIDI.py
+++ data_to_MIDI.py
@@ -1,5 +1,4 @@
-
-from mido import MidiFile, MidiTrack, Message, MetaMessage
+from mido import MidiFile, MidiTrack, Message, MetaMessage, bpm2tempo
 from MIDI_data_extractor import MIDI_data_extractor
 from tqdm import tqdm
 
@@ -9,23 +8,24 @@ def MIDI_data_creator(matrix, midi_file_path):
     tracks = {}
     tracks_t_time = {}
     # print(matrix)
-    '''[note_on_note, note_on_velocity, note_off_note, note_off_velocity, control_change_control, 
-    control_change_value, program_change_program, end_of_track, set_tempo_tempo, time_sig_num, itme_sig_den, 
-    time_sig_clocksperclick, time_sig_notated_32nd, key_sig(turn into numbers), [time], 
-    instrument_type, instrument_num, orig_instrument_number]'''
+    '''[note_on_note, note_on_velocity, note_off_note, note_off_velocity,
+        control_change_control, control_change_value, program_change_program,
+        end_of_track, set_tempo_tempo,
+        time_sig_num, itme_sig_den, time_sig_clocksperclick, time_sig_notated_32nd,
+        key_sig(turn into numbers), [time], instrument_type, instrument_num, orig_instrument_type]'''
     for i in tqdm(range(len(matrix))):
         cur_name = f"o{matrix[i][-1]}i{matrix[i][-2]}"
         # print(matrix[i])
         if cur_name == f"o-1i-1":
             cur_name = list(tracks.keys())[0]
-            time = matrix[i][14] - tracks_t_time[cur_name]
-            tracks_t_time[cur_name] = matrix[i][14]
-            if matrix[i][8] != -1:
-                tracks[cur_name].append(MetaMessage('set_tempo', tempo=matrix[i][8], time=time))
-            if matrix[i][9] != -1:
-                tracks[cur_name].append(MetaMessage('time_signature', numerator=matrix[i][9], denominator=matrix[i][10],
-                                                    clocks_per_click=matrix[i][11],
-                                                    notated_32nd_notes_per_beat=matrix[i][12], time=time))
+            time = matrix[i][-4] - tracks_t_time[cur_name]
+            tracks_t_time[cur_name] = matrix[i][-4]
+            if matrix[i][6] != -1:
+                tracks[cur_name].append(MetaMessage('set_tempo', tempo=bpm2tempo(20 + matrix[i][6]), time=time))
+            if matrix[i][7] != -1:
+                tracks[cur_name].append(MetaMessage('time_signature', numerator=matrix[i][7], denominator=matrix[i][8],
+                                                    clocks_per_click=matrix[i][9],
+                                                    notated_32nd_notes_per_beat=matrix[i][10], time=time))
         elif cur_name not in tracks:
             tracks_t_time[cur_name] = 0
             time = 0
@@ -33,24 +33,22 @@ def MIDI_data_creator(matrix, midi_file_path):
             tracks[cur_name].append(Message('program_change', program=matrix[i][-1], time=time))
 
         else:
-            time = matrix[i][14] - tracks_t_time[cur_name]
-            tracks_t_time[cur_name] = matrix[i][14]
+            time = matrix[i][-4] - tracks_t_time[cur_name]
+            tracks_t_time[cur_name] = matrix[i][-4]
             if matrix[i][0] != -1:
                 tracks[cur_name].append(Message('note_on', note=matrix[i][0], velocity=matrix[i][1], time=time))
             if matrix[i][2] != -1:
-                tracks[cur_name].append(Message('note_off', note=matrix[i][2], velocity=matrix[i][3], time=time))
+                tracks[cur_name].append(Message('control_change', control=matrix[i][2], value=matrix[i][3], time=time))
             if matrix[i][4] != -1:
-                tracks[cur_name].append(Message('control_change', control=matrix[i][4], value=matrix[i][5], time=time))
-            if matrix[i][6] != -1:
-                tracks[cur_name].append(Message('program_change', program=matrix[i][6], time=time))
-            if matrix[i][7] != -1:
+                tracks[cur_name].append(Message('program_change', program=matrix[i][4], time=time))
+            if matrix[i][5] != -1:
                 tracks[cur_name].append(MetaMessage('end_of_track', time=time))
-            if matrix[i][13] != -1:
+            if matrix[i][11] != -1:
                 key_sig_dict = {0: 'A', 1: 'A#m', 2: 'Ab', 3: 'Abm', 4: 'Am', 5: 'B', 6: 'Bb', 7: 'Bbm', 8: 'Bm',
                                 9: 'C', 10: 'C#', 11: 'C#m', 12: 'Cb', 13: 'Cm', 14: 'D', 15: 'D#m', 16: 'Db',
                                 17: 'Dm', 18: 'E', 19: 'Eb', 20: 'Ebm', 21: 'Em', 22: 'F', 23: 'F#', 24: 'F#m',
                                 25: 'Fm', 26: 'G', 27: 'G#m', 28: 'Gb', 29: 'Gm'}
-                tracks[cur_name].append(MetaMessage('key_signature', key=key_sig_dict[matrix[i][13]], time=time))
+                tracks[cur_name].append(MetaMessage('key_signature', key=key_sig_dict[matrix[i][11]], time=time))
     for track in tracks:
         midi_file.tracks.append(tracks[track])
     # print(midi_file)
@@ -58,7 +56,7 @@ def MIDI_data_creator(matrix, midi_file_path):
     midi_file.save(midi_file_path)
 
 
-input_file_path = r""
-output_file_path = r""
+input_file_path = r"C:\Users\ilove\Downloads\Metamorphosen_TrV_290__Richard_Strauss_Metamorphosen_1.13_MS4.01.mid"
+output_file_path = r"metaph.mid"
 data_0 = MIDI_data_extractor(input_file_path)
 MIDI_data_creator(data_0, output_file_path)
