cc7a11ea7fd9db06574a2cdf71fbbdd5db8719a0
Ryan Kim <ryankim17920@gmail.com>
2023-07-18 22:30:31 -0500

    A Failure, new feature, removed some midi
    Changes:
    - removed bach midi folder due to corruptness. I will add a new one.
    - added MIDI_merger to merge some tracks due to some light corruption in other MIDIs
    - tried to create another data to numbers and failed miserably due to there being like a few trillion or so possibilities (it would take 3 years to finish)

--- add_column_to_2d_array.py
+++ add_column_to_2d_array.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-# This function is currently deprecated, use index_based_matrix_appender instead.
-
 def add_column_to_2d_array(array, number):
     # Create a column with the same number of rows as the original array
     column = np.full((array.shape[0], 1), number)
