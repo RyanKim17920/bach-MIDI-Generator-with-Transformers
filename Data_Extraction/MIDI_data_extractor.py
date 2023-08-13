af0104b7f0950ab7cfd8ee2425d50225174142ff
Ryan Kim <ryankim17920@gmail.com>
2023-08-13 12:18:45 -0500

    Updated data_extractor
    Changes:
    - More space efficient and time efficient
    - Better ability to generalize
    - Comments!

--- Data_Extraction/MIDI_data_extractor.py
+++ Data_Extraction/MIDI_data_extractor.py
@@ -31,19 +31,58 @@ def add_column_to_2d_array(array, number):
     return np.append(array, column, axis=1)
 
 
+key_sig_dict = {'A': 0, 'A#m': 1, 'Ab': 2, 'Abm': 3, 'Am': 4, 'B': 5,
+                'Bb': 6, 'Bbm': 7, 'Bm': 8, 'C': 9, 'C#': 10, 'C#m': 11, 'Cb': 12, 'Cm': 13,
+                'D': 14, 'D#m': 15, 'Db': 16, 'Dm': 17, 'E': 18, 'Eb': 19, 'Ebm': 20, 'Em': 21, 'F': 22,
+                'F#': 23, 'F#m': 24, 'Fm': 25, 'G': 26, 'G#m': 27, 'Gb': 28, 'Gm': 29}
+
+
+# key signature dictionary mapping
+
+
+# Creation of a new, more efficient, more readable, and more compact data extractor
+
 def MIDI_data_extractor(midi_file_path,
                         verbose=0,
                         relative_time=True,
                         include_start=True,
                         include_end=True,
                         include_instr_type=True):
-    np.set_printoptions(threshold=np.inf)
+    """'
+    Inputs: MIDI file
+
+    Outputs: 2D array of MIDI data:
+        Size: (num_messages, 11)
+
+        index 0: type of data (start/end token, note_on, control_change, program_change, etc.)
+        index 1-6: data values (extra indexes included for future expansion)
+        index 7: time (relative or absolute)
+        index 8: current instrument type (removable)
+        index 9: instrument number (2nd violin for example)
+        index 10: original instrument type
+
+    Ordering of importance (for model):
+        1. start/end token
+        2. program change
+        3. control change
+        4. time signature
+        5. key signature
+        6. tempo
+        7. note_on
+
+        Notes:
+        --> adding new importance values is significantly easier than the old method
+        --> note_off is treated as note_off with velocity 0 to reduce data size
+    """
+    np.printoptions(threshold=np.inf)
     np.set_printoptions(linewidth=np.inf)
+    np.set_printoptions(threshold=np.inf)
     midi_file = MidiFile(midi_file_path)
     matrix = np.array([], dtype=np.int16)
     used_instruments = np.zeros(128)
     track_rp = -1
     organ_count = 0
+
     for i, track in tqdm(enumerate(midi_file.tracks), disable=False if verbose >= 1 else True):
         if verbose == 2:
             print('Track {}: {}'.format(i, track.name))
@@ -54,194 +93,171 @@ def MIDI_data_extractor(midi_file_path,
         orig_instr = -1
         for msg in track:
             msg_counter += 1
-            msg_array = np.full(13, -1)
+            msg_array = np.full(8, -1)
             cur_time += msg.time
             msg_array[-1] = cur_time
             # print(cur_time)
             # print(msg.type)
-            if msg.type == 'note_on':
-                msg_array[0:2] = msg.note, msg.velocity
-            elif msg.type == 'note_off':
-                msg_array[0:2] = msg.note, 0
-                # note_off is unnecessary and should be removed
-            # deleted 2-4 hmm
-            elif msg.type == 'control_change':
-                msg_array[2:4] = msg.control, msg.value
-            elif msg.type == 'program_change':
-                msg_array[4] = msg.program
-                # print(msg.program)
-                program_matrix = np.append(program_matrix, [[msg_counter], [msg.program]])
-                program_matrix = np.reshape(program_matrix, (-1, 2))
-                if orig_instr == -1:
-                    orig_instr = msg.program
-                if msg.program <= 8 and track_rp == -1:
-                    track_rp = msg.program
-                if 17 <= msg.program <= 24:
-                    organ_count += 2
-                    track_rp = msg.program
-            # elif msg.type == 'end_of_track':
-            # msg_array[5] = 0
-            elif msg.type == 'set_tempo':
-                st_array = np.full(16, -1)
-                st_array[-4] = cur_time
-                st_array[6] = tempo2bpm(msg.tempo)
-                matrix = np.append(matrix, [st_array])
-            elif msg.type == 'time_signature':
-                ts_array = np.full(16, -1)
-                ts_array[-4] = cur_time
-                ts_array[7:11] = msg.numerator, msg.denominator, msg.clocks_per_click, msg.notated_32nd_notes_per_beat
-                matrix = np.append(matrix, [ts_array])
-            elif msg.type == 'key_signature':
-                key_sig_dict = {'A': 0, 'A#m': 1, 'Ab': 2, 'Abm': 3, 'Am': 4, 'B': 5,
-                                'Bb': 6, 'Bbm': 7, 'Bm': 8, 'C': 9, 'C#': 10, 'C#m': 11, 'Cb': 12, 'Cm': 13,
-                                'D': 14, 'D#m': 15, 'Db': 16, 'Dm': 17, 'E': 18, 'Eb': 19, 'Ebm': 20, 'Em': 21, 'F': 22,
-                                'F#': 23, 'F#m': 24, 'Fm': 25, 'G': 26, 'G#m': 27, 'Gb': 28, 'Gm': 29}
-                msg_array[11] = key_sig_dict[msg.key]
+            match msg.type:
+                case 'program_change':
+                    msg_array[0] = 1
+                    # this is set to 1 due to its importance in the model
+                    # the programs (instruments) have to be put almost first in order for generation to function
+                    msg_array[1] = msg.program
+                    # print(msg.program)
+                    program_matrix = np.append(program_matrix, [[msg_counter], [msg.program]])
+                    program_matrix = np.reshape(program_matrix, (-1, 2))
+                    if orig_instr == -1:
+                        orig_instr = msg.program
+                        # tracking original_instrument
+                    if msg.program <= 8 and track_rp == -1:
+                        track_rp = msg.program
+                        # tracking track_rp, which includes possible multi-handed instruments
+                        # these can, for some reason, have no program_changes for one of the hand
+                        # thus, it is worth tracking the program_change for future usage if necessary
+                    if 17 <= msg.program <= 24:
+                        organ_count += 2
+                        track_rp = msg.program
+                        # same here, but for organs, which can have up to three hands
+                case 'control_change':
+                    msg_array[0] = 2
+                    # control changes are very important for the model
+                    msg_array[1:3] = msg.control, msg.value
+                case 'time_signature':
+                    # time signatures are global and also have importance
+                    ts_array = np.full(11, -1)
+                    ts_array[-4] = cur_time
+                    ts_array[0] = 3
+                    ts_array[
+                    1:5] = msg.numerator, msg.denominator, msg.clocks_per_click, msg.notated_32nd_notes_per_beat
+                    matrix = np.append(matrix, [ts_array])
+                    # global impact occurs here too
+                case 'key_signature':
+                    msg_array[0] = 4
+                    msg_array[1] = key_sig_dict[msg.key]
+                case 'set_tempo':
+                    st_array = np.full(11, -1)
+                    st_array[-4] = cur_time
+                    st_array[0] = 5
+                    st_array[1] = tempo2bpm(msg.tempo)
+                    matrix = np.append(matrix, [st_array])
+                    # appending to matrix, not track matrix, because this has a global impact (impacts all tracks)
+                case 'note_on':
+                    msg_array[0] = 6
+                    # this is set to this value (not like 1 or 0) due to order of importance in the model
+                    msg_array[1:3] = msg.note, msg.velocity
+                case 'note_off':
+                    msg_array[0] = 6
+                    msg_array[1:3] = msg.note, 0
+                    # note_off is often considered a note with zero velocity and can be removed for conciseness
+                    # implement later: toggle on/off for this
+
             if not np.all(msg_array[0:-1] == -1):
+                # checks if it's not emptpy, if it isn't, add to track_matrix
                 track_matrix = np.append(track_matrix, [msg_array])
+
         track_matrix = track_matrix.astype(np.int64)
-        if len(program_matrix) > 0:
-            if program_matrix.size != 0:
-                used_instruments[int(program_matrix[0][1])] += 1
-                instr_num = used_instruments[int(program_matrix[0][1])]
-            # print(instr_num)
-            else:
-                if track_rp != -1:
-                    program_matrix = np.append(program_matrix, [[0], [track_rp]])
-                    program_matrix = np.reshape(program_matrix, (-1, 2))
-                    msg_array = np.full(13, -1)
-                    msg_array[-1] = 0
-                    msg_array[4] = track_rp
-                    track_matrix = np.append(track_matrix, [msg_array])
-                    orig_instr = track_rp
-                    if track_rp <= 8:
+
+        if program_matrix.size == 0:
+            # if there are no programs, use the tracked instrument number for multi-hand instruments
+            if track_rp != -1:
+                # if track_rp exists, use that
+                program_matrix = np.append(program_matrix, [[0], [track_rp]])
+                program_matrix = np.reshape(program_matrix, (-1, 2))
+                msg_array = np.full(8, -1)
+                msg_array[-1] = 0
+                msg_array[0] = 1
+                msg_array[1] = track_rp
+                track_matrix = np.append(track_matrix, [msg_array])
+                orig_instr = track_rp
+                # remove track_rp if used up
+                if track_rp <= 8:
+                    track_rp = -1
+                elif 17 <= track_rp <= 24:
+                    organ_count -= 1
+                    if organ_count == 0:
                         track_rp = -1
-                    elif 17 <= track_rp <= 24:
-                        organ_count -= 1
-                        if organ_count == 0:
-                            track_rp = -1
-                else:
-                    program_matrix = np.append(program_matrix, [[0], [0]])
-                    program_matrix = np.reshape(program_matrix, (-1, 2))
-                    msg_array = np.full(13, -1)
-                    msg_array[-1] = 0
-                    track_matrix = np.append(track_matrix, [msg_array])
-                used_instruments[int(program_matrix[0][1])] += 1
-                instr_num = used_instruments[int(program_matrix[0][1])]
-            track_matrix = track_matrix.reshape((-1, 13))
-            track_matrix = index_based_matrix_appender(track_matrix, program_matrix)
-            track_matrix = add_column_to_2d_array(track_matrix, instr_num)
-            track_matrix = add_column_to_2d_array(track_matrix, orig_instr)
-            matrix = np.append(matrix, track_matrix)
-            matrix = matrix.reshape((-1, 16))
+            else:
+                # if doesn't exist just default to program 0 (piano)
+                program_matrix = np.append(program_matrix, [[0], [0]])
+                program_matrix = np.reshape(program_matrix, (-1, 2))
+                msg_array = np.full(8, -1)
+                msg_array[-1] = 0
+                msg_array[0] = 1
+                msg_array[1] = 0
+                track_matrix = np.append(track_matrix, [msg_array])
+                orig_instr = 0
+        # increment respective used_instruments and use that instrument number
+        used_instruments[int(program_matrix[0][1])] += 1
+        instr_num = used_instruments[int(program_matrix[0][1])]
 
+        track_matrix = track_matrix.reshape((-1, 8))
+        # reshape for appending
+        track_matrix = index_based_matrix_appender(track_matrix, program_matrix)
+        track_matrix = add_column_to_2d_array(track_matrix, instr_num)
+        track_matrix = add_column_to_2d_array(track_matrix, orig_instr)
+        # add program_matrix onto track_matrix for current instrument based on the message
+        # add instrument number and original instrument number to track_matrix
+        matrix = np.append(matrix, track_matrix)
+        # append track_matrix to matrix
+        matrix = matrix.reshape((-1, 11))
 
-        # print(track_matrix[0:100])
+    # print(track_matrix[0:100])
 
-    matrix = matrix.reshape((-1, 16))
+    # remove duplicates
     matrix = np.unique(matrix, axis=0)
     matrix = matrix.astype(np.int64)
+    # set to respective type
 
-
-
-    '''sorting starts here'''
-    # deprecated order: from first to last: Time, Program_change(Instrument), Tempo, time_sig, key_sig, control_change
-    cols_to_sort = [4, 6, 7, 11]
-
-    # Initialize an empty list to hold the new rows
-    new_rows = []
-
-    # Iterate over each row in your matrix
-    for row in matrix:
-        # Create a flag to check if a new row has been added
-        new_row_added = False
-        # Iterate over the columns you want to split
-        for col in cols_to_sort:
-            # Skip the iteration if the column is 12 (or -4)
-            if col == 12 or col == -4:
-                continue
-            # Create a new row that is a copy of the original row
-            new_row = row.copy()
-            # Check if the value in the column is not -1
-            if new_row[col] != -1:
-                this_time = new_row[-4]
-                # Set all other columns in cols_to_sort to -1
-                for i in cols_to_sort:
-                    if i != col:
-                        new_row[i] = -1
-                new_row[-4] = this_time
-                # Append the new row to the list of new rows
-                new_rows.append(new_row)
-                # Set the flag to True
-                new_row_added = True
-        # If no new row was added, append the original row
-        if not new_row_added:
-            new_rows.append(row)
-
-    # Use numpy.vstack to stack the list of new rows into a new matrix
-    matrix = np.vstack(new_rows)
-
-    # masking for the 5th index
-    mask = matrix[:, 5] == -1
-
-    matrix = np.concatenate((matrix[mask], matrix[~mask]))
-
-    # Then sort by column 4 (value 4) - put all -1 at the end
-    index_value_4 = np.argsort(matrix[:, 4] == -1, kind='stable')
-    matrix = matrix[index_value_4]
-
-    # Now sort by column -4 (time) in ascending order
-    index_time = np.argsort(matrix[:, -4], kind='stable')
-    matrix = matrix[index_time]
+    # sorting starts here
+    # sort by time, then by index 0 within the times in increasing order while preserving order
+    matrix = matrix[np.lexsort((matrix[:, 0], matrix[:, -4]))]
 
     if relative_time:
         tracks_t_time = {}
+        # track time for each track
         for i in tqdm(range(len(matrix)), disable=False if verbose >= 1 else True):
             try:
                 cur_name = f"o{matrix[i][-1]}i{matrix[i][-2]}"
+                # get the name of the instrument based on original instrument and instrument number
                 if cur_name == f"o-1i-1":
+                    # if it's a global message, just set it to the first instrument that exists
                     cur_name = list(tracks_t_time.keys())[0]
-                    time = matrix[i][-4] - tracks_t_time[cur_name]
-                    tracks_t_time[cur_name] = matrix[i][-4]
-                    matrix[i][-4] = time
                 else:
+                    # if it's not global message, set it to the instrument name
+                    # if it doesn't already exist, create it with initialized time of 0
                     if cur_name not in tracks_t_time:
                         tracks_t_time[cur_name] = 0
-                    time = matrix[i][-4] - tracks_t_time[cur_name]
-                    tracks_t_time[cur_name] = matrix[i][-4]
-                    matrix[i][-4] = time
-            except:
+
+                time = matrix[i][-4] - tracks_t_time[cur_name]
+                # get the time difference between the current time and the last time
+                tracks_t_time[cur_name] = matrix[i][-4]
+                # set the last time to the current time
+                matrix[i][-4] = time
+                # set the time to the time difference
+            except IndexError:
                 pass
-        if include_end:
-            end_track = np.full(16, -1)
-            end_track[-4] = 0
-            end_track[5] = 1
-            matrix = np.append(matrix, [end_track])
-            matrix = matrix.reshape((-1, 16))
-        if include_start:
-            start_track = np.full(16, -1)
-            start_track[-4] = 0
-            start_track[5] = 0
-            matrix = np.append([start_track], matrix)
-            matrix = matrix.reshape((-1, 16))
-    else:
-        if include_end:
-            end_track = np.full(16, -1)
-            end_track[-4] = matrix[len(matrix) - 1][-4]
-            end_track[5] = 1
-            matrix = np.append(matrix, [end_track])
-            matrix = matrix.reshape((-1, 16))
-        if include_start:
-            start_track = np.full(16, -1)
-            start_track[-4] = 0
-            start_track[5] = 0
-            matrix = np.append([start_track], matrix)
-            matrix = matrix.reshape((-1, 16))
-    '''[note_on_note, note_on_velocity,
-        control_change_control, control_change_value, program_change_program,
-        end/start_marking, set_tempo_tempo,
-        time_sig_num, time_sig_den, time_sig_clocksperclick, time_sig_notated_32nd,
-        key_sig(turn into numbers), [time], (optional: instrument_type), instrument_num, orig_instrument_type]'''
+    if include_end:
+        end_track = np.full(11, -1)
+        end_track[-4] = 0 if relative_time else matrix[len(matrix) - 1][-4]
+        # 0 if it is relative time (right after last event), else the last time
+        end_track[1] = 1
+        end_track[0] = 0
+        # greatest importance!
+        matrix = np.append(matrix, [end_track])
+        # append to end of matrix
+        matrix = matrix.reshape((-1, 11))
+    if include_start:
+        start_track = np.full(11, -1)
+        start_track[-4] = 0
+        # occurs at time 0 regardless of relative time or not
+        start_track[1] = 0
+        start_track[0] = 0
+        matrix = np.append([start_track], matrix)
+        # append to start of matrix
+        matrix = matrix.reshape((-1, 11))
     if not include_instr_type:
-        matrix = np.delete(matrix, 13, 1)
+        matrix = np.delete(matrix, 8, 1)
     return matrix
+
+print(MIDI_data_extractor(r"C:\Users\ilove\CODING\PYStuff\MusicNet\Midi2Numpy\MIDI-Generator-with-Transformers\Bach MIDIs\Organ Works\An Wasserflussen Babylon - BWV 653 (By the Waters of Babylon).mid"))
